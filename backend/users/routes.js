a5d188e1bc3c9abc7900e5f2eaa5f6a91c9da310
OrionLeUMass <ole@umass.edu>
2025-10-03 14:17:38 -0400

    made get and post requests

--- backend/users/routes.js
+++ backend/users/routes.js
@@ -1,1 +1,25 @@
 // this contains all the user routes
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { getUserTable, postUserTable } from "./services.js";
+const router = express.Router();
+router.get("/api/users", async (req, res) => {
+    try {
+        const users = await getUserTable();
+        res.json(users);
+    } 
+    catch (error) {
+        console.error("Error fetching users:", error);
+    }
+});
+router.post("/api/users", async (req, res) => {
+    try {
+        const user = req.body; // Access the user data sent in the request body
+        const newUser = await postUserTable(user); // Save the user to the database
+        res.json(newUser); // Respond with the newly created user
+    } 
+    catch (error) {
+        console.error("Error creating user:", error);
+    }
+});
--- backend/users/services.js
+++ backend/users/services.js
@@ -1,1 +1,28 @@
 // this contains all the logic that encapsulates user operations
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+export async function getUserTable(){
+    const result = await supabase.from("user_table").select("*");
+    if(result.error || !result.data){
+        throw new Error("Error getting user data.");
+    }
+    else{
+        userData = result.data;
+        return userData;
+    }
+}
+export async function postUserTable(user){
+    const result = await supabase.from("user_table").insert(user).select();
+    if(result.error || !result.data){
+        throw new Error("Error posting user data.");
+    }
+    else{
+        postedData = result.data;
+        return postedData[0];
+    }
+}
