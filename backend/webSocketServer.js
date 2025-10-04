ac8e3a9d1101ed40e5f73bc4bd72683497fb2daf
EdisonZheng2323 <edisonzheng3737@gmail.com>
2025-10-03 22:27:12 -0400

    Set up WebSockets server

---
+++ backend/webSocketServer.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 8080});
+
+wss.on("connection", (ws) => {
+  console.log("New client connected!");
+
+  ws.on("message", (message) => {
+    console.log("Message recieved!");
+  });
+
+  ws.on("close", () => {
+    
+  })
+});
+
