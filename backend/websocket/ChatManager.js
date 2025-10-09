4925fee00a86ccc4cb0642df455323c0ce4f41da
abhijit <kamathabhijit2002@gmail.com>
2025-10-08 20:25:50 -0400

    adding supabase implementation

--- backend/websocket/ChatManager.js
+++ backend/websocket/ChatManager.js
@@ -1,3 +1,4 @@
+import { supabase } from "../utils/supabase.js";
 export class ChatManager {
   static instance = null;
 
@@ -23,7 +24,7 @@ export class ChatManager {
     });
   }
 
-  handleConnection(ws, req) {
+  async handleConnection(ws, req) {
     const urlParts = req.url.split("/");
     const chatId = urlParts[urlParts.length - 1];
 
@@ -32,7 +33,24 @@ export class ChatManager {
       return;
     }
 
-    // TODO: check if this is a valid chatId with database before persisting it within node memory
+    try {
+      // Check if this is a valid chatId with database before persisting it within node memory
+      const { data, error } = await supabase
+        .from("chats")
+        .select("*")
+        .eq("id", chatId)
+        .single();
+
+      if (error) {
+        console.error(`Invalid chat ID ${chatId}:`, error);
+        ws.close(1008, "chat id not found");
+        return;
+      }
+    } catch (err) {
+      console.error(`Error validating chat ID ${chatId}:`, err);
+      ws.close(1011, "Internal server error");
+      return;
+    }
 
     // Create room if doesn't exist
     if (!this.chatRooms.has(chatId)) {
@@ -53,7 +71,7 @@ export class ChatManager {
     });
   }
 
-  handleMessage(ws, room, chatId, data) {
+  async handleMessage(ws, room, chatId, data) {
     try {
       const message = JSON.parse(data);
 
@@ -61,14 +79,27 @@ export class ChatManager {
       console.log("time: ", message.time);
       console.log("message: ", message.message);
 
-      // Broadcast to everyone in the same chat
+      const {data, error} = await supabase.from("messages").insert({
+        chat_id: chatId,
+        user: message.user_id,
+        timestamp: message.timestamp,
+        content: message.message,
+      });
+
+      if (error) {
+        console.error("Error saving message to database:", error);
+        ws.close(1011, "Internal server error");
+        return;
+      }
+
+      // Broadcast to everyone in the same chat after we have saved to the database
       for (const client of room) {
         if (client.readyState === ws.OPEN) {
           client.send(JSON.stringify({ chatId, ...message }));
         }
       }
 
-      // TODO: save message to database here
+      
     } catch (err) {
       console.error("Invalid message:", err);
     }
