cf3cf11f41d6656faa967552aa77b58669a3d658
abhijit <kamathabhijit2002@gmail.com>
2025-10-10 15:22:31 -0700

    adding spread operator to compress and remove the nested object returned by the join

--- backend/messages/controller.js
+++ backend/messages/controller.js
@@ -1,14 +1,18 @@
 import { supabase } from "../supabase-client.js";
 
-export async function getMessage(chatId, startDate,endDate) {
-    const {data, error} = await supabase.from("messages").select(`
+export async function getMessage(chatId, startDate, endDate) {
+  const { data, error } = await supabase
+    .from("messages")
+    .select(
+      `
         timestamp,
         content,
-        users (name)
-        `)
-        .eq("chat_id", chatId)
-        .gte("timestamp", startDate)
-        .lte("timestamp", endDate)
-        .order("timestamp", {ascending: true})
-    return {data, error}
+        ...users(name)
+        `
+    )
+    .eq("chat_id", chatId)
+    .gte("timestamp", startDate)
+    .lte("timestamp", endDate)
+    .order("timestamp", { ascending: true });
+  return { data, error };
 }
