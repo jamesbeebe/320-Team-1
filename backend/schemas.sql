aef4015a850484ab91df9424a711ca3a9a939bb2
abhijit <kamathabhijit2002@gmail.com>
2025-10-14 18:48:40 -0400

    fixing up schema to user auth.users table rather than a custom users table

--- backend/schemas.sql
+++ backend/schemas.sql
@@ -1,11 +1,3 @@
-CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-
 CREATE TABLE chats (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255),
@@ -20,5 +12,5 @@ CREATE TABLE messages (
     content TEXT,
     
     FOREIGN KEY (chat_id) REFERENCES chats(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES auth.users(id)
 );
