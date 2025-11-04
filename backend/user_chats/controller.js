ba726a633126380df349940a6e1864a99011ff0e
EdisonZheng2323 <edisonzheng3737@gmail.com>
2025-11-03 22:46:31 -0500

    Create route that returns all chats a user belongs to for a specific class

--- backend/user_chats/controller.js
+++ backend/user_chats/controller.js
@@ -3,10 +3,24 @@ import { supabase } from "../supabase-client.js";
 export async function createUserChat(chatId, userId) {
   const { data, error } = await supabase
     .from("user_chats")
-    .insert({ chat_id: chatId, userId: userId })
+    .insert({ chat_id: chatId, user_id: userId })
     .select()
     .single();
   return { data, error };
+}
+
+export async function getAllUserChatsForSpecificClass(classId, userId){
+  const {data, error} = await supabase
+    .from("user_chats")
+    .select(`
+      chats(id,
+      name,
+      type
+      )
+      `)
+    .eq("user_id", userId)
+    .eq("chats.class_id", classId);
+  return {data, error};
 }
 
 export async function deleteUserChat(chatId, userId) {
